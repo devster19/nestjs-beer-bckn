6c0235335eaa793d80bddcdf08ca2570b8cca0be
Kittisak <k.ardkonghan@gmail.com>
2020-05-17 13:16:07 +0700

    first commit

---
+++ src/beers/dto/create-beer.dto.ts
@@ -0,0 +1,6 @@
+export class CreateBeerDto {
+    id: string
+    name: string;
+    country: string;
+    abv: number;
+}
---
+++ src/beers/interfaces/beer.interface.ts
@@ -0,0 +1,6 @@
+export interface Beer {
+    id?:string,
+    name: string;
+    country: string;
+    abv: number;
+}
